d12fdff7f227bff1c4dc3a7aa905cc398ab51b12
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2024-10-21 23:14:22 -0700

    Adds vulkan validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,40 @@
 #include <cstdlib>
-#include <iostream>
 #include <optional>
 #include <stdexcept>
 #include <vector>
+
+#include "plog/Appenders/ColorConsoleAppender.h"
+#include "plog/Formatters/TxtFormatter.h"
+#include "plog/Severity.h"
+#include "plog/Initializers/RollingFileInitializer.h"
+
+// Disable implicit fallthrough warning
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#include "plog/Log.h"
+
+#include "vulkan/vulkan_core.h"
 
 #include <vulkan/vulkan.hpp>
 #include <SDL3/SDL.H>
 #include <SDL3/SDL_vulkan.h>
 
+
+PFN_vkCreateDebugUtilsMessengerEXT  pfnVkCreateDebugUtilsMessengerEXT;
+PFN_vkDestroyDebugUtilsMessengerEXT pfnVkDestroyDebugUtilsMessengerEXT;
+
 #define APP_SHORT_NAME "Game"
 #define ENGINE_NAME "Liebeskind"
+
+#ifndef DEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
 
 class SDLException : private std::runtime_error {
 public:
@@ -42,11 +67,25 @@ public:
     }
 };
 
+
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT( VkInstance                                 instance,
+                                                               const VkDebugUtilsMessengerCreateInfoEXT * pCreateInfo,
+                                                               const VkAllocationCallbacks *              pAllocator,
+                                                               VkDebugUtilsMessengerEXT *                 pMessenger )
+{
+  return pfnVkCreateDebugUtilsMessengerEXT( instance, pCreateInfo, pAllocator, pMessenger );
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT( VkInstance instance, VkDebugUtilsMessengerEXT messenger, VkAllocationCallbacks const * pAllocator )
+{
+  return pfnVkDestroyDebugUtilsMessengerEXT( instance, messenger, pAllocator );
+}
+
 std::optional<std::vector<const char*>> getInstanceExtensions() {
     uint32_t count;
     const char* const* extensions = SDL_Vulkan_GetInstanceExtensions(&count);
 
-    std::cout << "Query for instance extensions yields:" << std::endl;
+    PLOG_INFO << "Query for instance extensions yields:";
 
     if (!extensions) {
         return {};
@@ -54,16 +93,87 @@ std::optional<std::vector<const char*>> getInstanceExtensions() {
 
     std::vector<const char*> allExtensions;
     for (uint32_t i = 0; i < count; i++) {
-        std::cout << i << ". " << extensions[i] << std::endl;
+        PLOG_INFO << "\t" << i << ". " << extensions[i];
         allExtensions.emplace_back(extensions[i]);
     }
 
+    PLOG_INFO << "\t" << count << ". " << VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
     allExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    if (enableValidationLayers) {
+        PLOG_INFO << "\t" << count << ". " << VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+        allExtensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
 
     return allExtensions;
 }
 
+bool areValidationLayersSupported() {
+    const std::vector<vk::LayerProperties> layerProperties = vk::enumerateInstanceLayerProperties();
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = 0;
+
+        for (const vk::LayerProperties& layerProperty : layerProperties)
+            layerFound |= std::strcmp(layerProperty.layerName, layerName) == 0;
+
+        if (!layerFound) 
+            return false;
+    }
+
+    return true;
+}
+
+bool initializeLogger() {
+	// TODO: create log file
+    static plog::ColorConsoleAppender<plog::TxtFormatter> appender;
+    plog::init(plog::Severity::debug, "liebeskind_runtime.log").addAppender(&appender);
+    PLOG_INFO << "Logger initialized.";
+	return true;
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugUtilsMessengerCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData
+) {
+    switch (messageSeverity) {
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            PLOG_VERBOSE << "VulkanValidation: " << pCallbackData->pMessage;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            PLOG_INFO << "VulkanValidation: " << pCallbackData->pMessage;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            PLOG_WARNING << "VulkanValidation: " << pCallbackData->pMessage;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            PLOG_ERROR << "VulkanValidation: " << pCallbackData->pMessage;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT:
+            PLOG_FATAL << "VulkanValidation: " << pCallbackData->pMessage;
+    }
+
+    return VK_FALSE;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pCallback) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pCallback);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT callback, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, callback, pAllocator);
+    }
+}
+
 int main() {
+    if (!initializeLogger())
+        throw SDLException("Logger can't start");
+
     SDL sdl(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
     VulkanLibrary vulkan_library(nullptr);
     SDL_Window* window = SDL_CreateWindow("Liebeskind", 1024, 768, SDL_WINDOW_VULKAN);
@@ -76,33 +186,62 @@ int main() {
         VK_API_VERSION_1_3
     );
 
+
     std::optional<std::vector<const char*>> instanceExtensions = getInstanceExtensions();
 
     if (instanceExtensions == std::nullopt)
         throw SDLException("No supported extensions found");
 
+    if (enableValidationLayers && !areValidationLayersSupported())
+        throw std::runtime_error("Validation layers requested but not available");
+
     const vk::InstanceCreateInfo instanceInfo(
         vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR,
         &appInfo,
-        0,
-        nullptr,
+        enableValidationLayers ? static_cast<uint32_t>(validationLayers.size()) : 0,
+        enableValidationLayers ? validationLayers.data() : nullptr,
         instanceExtensions->size(),
         instanceExtensions->data()
     );
 
     vk::Instance instance;
+    vk::DebugUtilsMessengerEXT debugUtilsMessenger;
+
     try {
         instance = vk::createInstance(instanceInfo, nullptr);
+
+        pfnVkCreateDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>( instance.getProcAddr( "vkCreateDebugUtilsMessengerEXT" ) );
+        pfnVkDestroyDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>( instance.getProcAddr( "vkDestroyDebugUtilsMessengerEXT" ) );
+
+        if (enableValidationLayers) {
+            vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+            vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(
+                    vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                    vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+                    vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+
+            debugUtilsMessenger = instance.createDebugUtilsMessengerEXT(
+                vk::DebugUtilsMessengerCreateInfoEXT({}, severityFlags, messageTypeFlags, &debugUtilsMessengerCallback));
+        }
+
     } catch (vk::SystemError& err) {
-        std::cout << "vk::SystemError: " << err.what() << std::endl;
+        PLOG_ERROR << "vk::SystemError: " << err.what();
         return EXIT_FAILURE;
     } catch (std::exception& err) {
-        std::cout << "std::exception: " << err.what() << std::endl;
+        PLOG_ERROR << "std::exception: " << err.what();
         return EXIT_FAILURE;
     } catch (...) {
-        std::cout << "Unknown error" << std::endl;
+        PLOG_ERROR << "Unknown error";
         return EXIT_FAILURE;
     }
+
+
+    PLOG_INFO << "Available Extensions:";
+
+    for (const auto& extension: vk::enumerateInstanceExtensionProperties())
+        PLOG_INFO << "\t" << extension.extensionName;
 
     while (true) {
         SDL_Event sdl_event;
@@ -115,10 +254,13 @@ int main() {
         if (shouldQuitGame) break;
     }
 
-    instance.destroy();
+    if (enableValidationLayers) instance.destroyDebugUtilsMessengerEXT(debugUtilsMessenger);
 
+    instance.destroy();
 
     SDL_DestroyWindow(window);
 
     return EXIT_SUCCESS;
 }
+
+#pragma GCC diagnostic pop

934d8b9b6e5ae9dd4e23e298d0834ab4563df509
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2025-04-18 00:00:54 -0700

    Add generationIndexArray structure

---
+++ src/engine/include/core/algo/generation_index_array.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <array>
+#include <span>
+#include <vector>
+#include "core/logger/assert.h"
+
+namespace algo {
+
+struct GenerationIndexPair {
+	uint16_t index;
+	uint16_t generation;
+};
+
+template <size_t N>
+struct GenerationIndexArray {
+	std::array<uint16_t, N> generation;
+	std::vector<uint16_t> free;
+
+	static_assert(N <= (1 << 16), "N is too large, keep it under 2^{16}");
+
+   public:
+	static GenerationIndexArray create();
+};
+
+template <size_t N>
+[[nodiscard]]
+GenerationIndexPair pushBack(GenerationIndexArray<N>& array);
+
+template <size_t N>
+std::vector<uint16_t> getLiveIndices(const GenerationIndexArray<N>& array);
+
+template <size_t N>
+inline bool isIndexValid(
+	const GenerationIndexArray<N>& array, GenerationIndexPair index
+) {
+	ASSERT(
+		index.index < N,
+		"Index " << index.index << " exceeds the capacity of " << N
+	);
+	return array.generation[index.index] == index.generation;
+}
+
+template <size_t N>
+void destroy(
+	GenerationIndexArray<N>& array, std::span<const GenerationIndexPair> indices
+);
+
+}  // namespace algo
---
+++ src/engine/src/core/algo/generation_index_array.cpp
@@ -0,0 +1,62 @@
+#include "core/algo/generation_index_array.h"
+
+#include "core/logger/assert.h"
+
+namespace algo {
+
+template <size_t N>
+GenerationIndexArray<N> GenerationIndexArray<N>::create() {
+	const std::vector<uint16_t> free = [&]() {
+		std::vector<uint16_t> result;
+		result.reserve(N);
+		for (uint16_t i = N - 1; i >= 0; i--) result.push_back(i);
+		return result;
+	}();
+	return {.generation = {}, .free = free};
+}
+
+template <size_t N>
+GenerationIndexPair pushBack(GenerationIndexArray<N>& array) {
+	ASSERT(
+		!array.free.empty(),
+		"Exceeding the capacity of the generation index array (" << N << ")"
+	);
+	const uint16_t index = array.free.back();
+	array.free.pop_back();
+	const uint16_t generation = array.generation[index];
+	return {
+		.index = index,
+		.generation = generation,
+	};
+}
+
+template <size_t N>
+std::vector<uint16_t> getLiveIndices(const GenerationIndexArray<N>& array) {
+    std::array<bool, N> isFree;
+    std::fill(isFree.begin(), isFree.end(), false);
+    for (const int& freedIndex : array.free)
+        isFree[freedIndex] = true;
+    std::vector<uint16_t> liveIndices;
+    liveIndices.reserve(N - array.free.size());
+    for (size_t i = 0; i < N; i++) {
+        if (!isFree[i])
+            liveIndices.push_back(i);
+    }
+    return liveIndices;
+}
+
+template <size_t N>
+void destroy(
+	GenerationIndexArray<N>& array, std::span<const GenerationIndexPair> indices
+) {
+	for (const GenerationIndexPair& index : indices) {
+		ASSERT(isIndexValid(array, index), "Cannot destroy an invalid entry");
+		array.generation[index.index]++;
+		array.free.push_back(index.index);
+	}
+}
+
+}  // namespace algo
+
+#include "low_level_renderer/meshes.h"
+template struct algo::GenerationIndexArray<graphics::MAX_MESHES>;
